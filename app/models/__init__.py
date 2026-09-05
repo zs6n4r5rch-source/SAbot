ad191fe314a78b66c942f226dc7f26405b0c6e52
zs6n4r5rch-source <zs6n4r5rch@privaterelay.appleid.com>
2026-09-05 17:41:27 +0300

    Fix broken model exports on production main

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,22 +1,16 @@
 from app.models.base import (
     Base,
-
     UserRole,
-
     Club,
     Employee,
     EmployeeClub,
-
     TelegramUser,
     AccessProfile,
     TelegramBindingRequest,
-
     Shift,
     ShiftStatus,
-
     ProductCategory,
     Product,
-
     Inventory,
     InventoryItem,
     InventoryBalance,
@@ -24,64 +18,32 @@ from app.models.base import (
     InventoryOperationType,
     InventoryStatus,
     StockSnapshot,
-
     Writeoff,
     WriteoffItem,
     WriteoffStatus,
     WriteoffReason,
-
     Discrepancy,
     DiscrepancyStatus,
-
-    ShiftCloseReport,
-    ShiftCloseReportStatus,
-    ShiftCloseStockItem,
-
-    SalaryRule,
-    SalaryPeriod,
-    SalaryAdjustment,
     SalaryViolation,
-    SalaryPayment,
-    NonMonetaryBonus,
-
-    Guest,
-    GuestGroup,
-    GuestGroupMember,
-    GuestTelegram,
-    GuestLinkToken,
-
-    MarketingCampaign,
-    MarketingCampaignGroup,
-    MarketingRecipient,
-    CampaignStatus,
-    RecipientStatus,
-
-    OwnerReportSettings,
-    OwnerDailyReportDelivery,
-
+    BonusRecord,
     AuditLog,
+    LangameSyncLog,
 )
 
 
 __all__ = [
     "Base",
-
     "UserRole",
-
     "Club",
     "Employee",
     "EmployeeClub",
-
     "TelegramUser",
     "AccessProfile",
     "TelegramBindingRequest",
-
     "Shift",
     "ShiftStatus",
-
     "ProductCategory",
     "Product",
-
     "Inventory",
     "InventoryItem",
     "InventoryBalance",
@@ -89,40 +51,14 @@ __all__ = [
     "InventoryOperationType",
     "InventoryStatus",
     "StockSnapshot",
-
     "Writeoff",
     "WriteoffItem",
     "WriteoffStatus",
     "WriteoffReason",
-
     "Discrepancy",
     "DiscrepancyStatus",
-
-    "ShiftCloseReport",
-    "ShiftCloseReportStatus",
-    "ShiftCloseStockItem",
-
-    "SalaryRule",
-    "SalaryPeriod",
-    "SalaryAdjustment",
     "SalaryViolation",
-    "SalaryPayment",
-    "NonMonetaryBonus",
-
-    "Guest",
-    "GuestGroup",
-    "GuestGroupMember",
-    "GuestTelegram",
-    "GuestLinkToken",
-
-    "MarketingCampaign",
-    "MarketingCampaignGroup",
-    "MarketingRecipient",
-    "CampaignStatus",
-    "RecipientStatus",
-
-    "OwnerReportSettings",
-    "OwnerDailyReportDelivery",
-
+    "BonusRecord",
     "AuditLog",
+    "LangameSyncLog",
 ]
