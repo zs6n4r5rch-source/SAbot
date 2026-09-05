70de84d9293cb8aaf7b1e989217022bd5f4fde12
zs6n4r5rch-source <zs6n4r5rch@privaterelay.appleid.com>
2026-09-05 09:47:29 +0300

    Export isolated salary models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,128 +1,29 @@
 from app.models.base import (
-    Base,
-
-    UserRole,
-
-    Club,
-    Employee,
-    EmployeeClub,
-
-    TelegramUser,
-    AccessProfile,
-    TelegramBindingRequest,
-
-    Shift,
-    ShiftStatus,
-
-    ProductCategory,
-    Product,
-
-    Inventory,
-    InventoryItem,
-    InventoryBalance,
-    InventoryOperation,
-    InventoryOperationType,
-    InventoryStatus,
-    StockSnapshot,
-
-    Writeoff,
-    WriteoffItem,
-    WriteoffStatus,
-    WriteoffReason,
-
-    Discrepancy,
-    DiscrepancyStatus,
-
-    ShiftCloseReport,
-    ShiftCloseReportStatus,
-    ShiftCloseStockItem,
-
-    SalaryRule,
-    SalaryPeriod,
-    SalaryAdjustment,
-    SalaryViolation,
-    SalaryPayment,
+    Base, UserRole, Club, Employee, EmployeeClub, TelegramUser, AccessProfile,
+    TelegramBindingRequest, Shift, ShiftStatus, ProductCategory, Product,
+    Inventory, InventoryItem, InventoryBalance, InventoryOperation,
+    InventoryOperationType, InventoryStatus, StockSnapshot, Writeoff, WriteoffItem,
+    WriteoffStatus, WriteoffReason, Discrepancy, DiscrepancyStatus, Guest, GuestGroup,
+    GuestGroupMember, GuestTelegram, GuestLinkToken, MarketingCampaign,
+    MarketingCampaignGroup, MarketingRecipient, CampaignStatus, RecipientStatus,
+    OwnerReportSettings, OwnerDailyReportDelivery, AuditLog,
+)
+from app.models.salary import (
+    ShiftCloseReport, ShiftCloseReportStatus, ShiftCloseStockItem,
+    SalaryRule, SalaryPeriod, SalaryAdjustment, SalaryViolation, SalaryPayment,
     NonMonetaryBonus,
-
-    Guest,
-    GuestGroup,
-    GuestGroupMember,
-    GuestTelegram,
-    GuestLinkToken,
-
-    MarketingCampaign,
-    MarketingCampaignGroup,
-    MarketingRecipient,
-    CampaignStatus,
-    RecipientStatus,
-
-    OwnerReportSettings,
-    OwnerDailyReportDelivery,
-
-    AuditLog,
 )
 
-
 __all__ = [
-    "Base",
-
-    "UserRole",
-
-    "Club",
-    "Employee",
-    "EmployeeClub",
-
-    "TelegramUser",
-    "AccessProfile",
-    "TelegramBindingRequest",
-
-    "Shift",
-    "ShiftStatus",
-
-    "ProductCategory",
-    "Product",
-
-    "Inventory",
-    "InventoryItem",
-    "InventoryBalance",
-    "InventoryOperation",
-    "InventoryOperationType",
-    "InventoryStatus",
-    "StockSnapshot",
-
-    "Writeoff",
-    "WriteoffItem",
-    "WriteoffStatus",
-    "WriteoffReason",
-
-    "Discrepancy",
-    "DiscrepancyStatus",
-
-    "ShiftCloseReport",
-    "ShiftCloseReportStatus",
-    "ShiftCloseStockItem",
-
-    "SalaryRule",
-    "SalaryPeriod",
-    "SalaryAdjustment",
-    "SalaryViolation",
-    "SalaryPayment",
-    "NonMonetaryBonus",
-
-    "Guest",
-    "GuestGroup",
-    "GuestGroupMember",
-    "GuestTelegram",
-    "GuestLinkToken",
-
-    "MarketingCampaign",
-    "MarketingCampaignGroup",
-    "MarketingRecipient",
-    "CampaignStatus",
-    "RecipientStatus",
-
-    "OwnerReportSettings",
-    "OwnerDailyReportDelivery",
-
+    "Base", "UserRole", "Club", "Employee", "EmployeeClub", "TelegramUser",
+    "AccessProfile", "TelegramBindingRequest", "Shift", "ShiftStatus",
+    "ProductCategory", "Product", "Inventory", "InventoryItem", "InventoryBalance",
+    "InventoryOperation", "InventoryOperationType", "InventoryStatus", "StockSnapshot",
+    "Writeoff", "WriteoffItem", "WriteoffStatus", "WriteoffReason", "Discrepancy",
+    "DiscrepancyStatus", "ShiftCloseReport", "ShiftCloseReportStatus", "ShiftCloseStockItem",
+    "SalaryRule", "SalaryPeriod", "SalaryAdjustment", "SalaryViolation", "SalaryPayment",
+    "NonMonetaryBonus", "Guest", "GuestGroup", "GuestGroupMember", "GuestTelegram",
+    "GuestLinkToken", "MarketingCampaign", "MarketingCampaignGroup", "MarketingRecipient",
+    "CampaignStatus", "RecipientStatus", "OwnerReportSettings", "OwnerDailyReportDelivery",
     "AuditLog",
 ]
