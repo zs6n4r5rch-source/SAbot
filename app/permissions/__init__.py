fa9ac78374e4c19479064871c129408911b0796c
zs6n4r5rch-source <zs6n4r5rch@privaterelay.appleid.com>
2026-09-08 23:29:58 +0300

    feat: add centralized permissions package

---
+++ app/permissions/__init__.py
@@ -0,0 +1,3 @@
+from app.permissions.core import Permission, ROLE_PERMISSIONS, has_permission, require_permission
+
+__all__ = ["Permission", "ROLE_PERMISSIONS", "has_permission", "require_permission"]
