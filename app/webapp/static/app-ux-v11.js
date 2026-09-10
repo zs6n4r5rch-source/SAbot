578fe3b3981c5300101a146a1d87013334800009
zs6n4r5rch-source <zs6n4r5rch@privaterelay.appleid.com>
2026-09-10 19:15:16 +0300

    Load bonuses workspace after functional app shell

--- app/webapp/static/app-ux-v11.js
+++ app/webapp/static/app-ux-v11.js
@@ -1,4 +1,8 @@
 (function(){'use strict';
 /* Live dashboard refresh is owned by app-guard.js. This layer intentionally adds no competing polling. */
+(function loadV12(){
+  if(document.querySelector('script[data-sa-ux-v12]'))return;
+  const s=document.createElement('script');s.src='/static/app-ux-v12.js?v=1';s.async=false;s.dataset.saUxV12='1';document.head.appendChild(s);
+})();
 window.__SA_UX_V11_READY__=true;
 })();
